51152385d10c7be05a54e96fbb23fac8d7cbb3da
Kris Jenkins <krisajenkins@gmail.com>
2013-05-06 10:02:21 +0100

    Creating a task to compile src/**/*.crisp to build/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,17 @@ module.exports = function(grunt) {
 					'should'
 				]
 			}
+		},
+		crisp: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: '**/*.crisp',
+					dest: 'build/',
+					ext: '.js'
+				}]
+			}
 		}
 	});
 
@@ -27,6 +38,24 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-cafe-mocha');
 	grunt.loadNpmTasks('grunt-beautify');
 
+	grunt.registerMultiTask('crisp', "Compile crisp files to JavaScript", function () {
+		grunt.config.requires('crisp.build');
+		var compiler = require('./lib/compiler');
+
+		grunt.log.writeln("Compiling crisps:");
+		this.files.forEach(function (file) {
+			if (file.src.length !== 1) {
+				grunt.fatal("More than one source file found. don't know why." + file.src);
+			}
+
+			var src = file.src[0],
+				dest = file.dest;
+
+			grunt.log.writeln("\tCompiling", src, "to", dest);
+			compiler.compile_io(src, dest);
+		});
+	});
+
 	// Default task(s).
-	grunt.registerTask('default', ['cafemocha', 'uglify']);
+	grunt.registerTask('default', ['cafemocha', 'crisp', 'uglify']);
 };
