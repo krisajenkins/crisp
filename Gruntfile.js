eb4c1ad2b128804ab3fb62d6f0eab953354ef993
Kris Jenkins <krisajenkins@gmail.com>
2013-05-20 12:27:41 +0100

    Adding browserify, uglify and Closure-compiler configurations.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,28 @@ module.exports = function (grunt) {
 	grunt.option('stack', true);
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			'build/browser.js': ['build/compiler.js']
+		},
+		'closure-compiler': {
+			build: {
+				closurePath: '/Users/kris/Work/Sketches/crisp/',
+				js: 'build/browser.js',
+				jsOutputFile: 'build/browser.min.js',
+				maxBuffer: 500,
+				options: {
+					compilation_level: 'ADVANCED_OPTIMIZATIONS',
+					language_in: 'ECMASCRIPT5_STRICT'
+				}
+			}
+		},
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
 			build: {
-				src: 'build/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+				src: 'build/browser.js',
+				dest: 'build/browser.min.js'
 			}
 		},
 		cafemocha: {
@@ -63,6 +78,8 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-cafe-mocha');
 	grunt.loadNpmTasks('grunt-beautify');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-closure-compiler');
 
 	grunt.registerMultiTask('crisp', "Compile crisp files to JavaScript", function () {
 		var compiler = require('./lib/compiler'),
